d82d6d709a33275d1499685ce85088d6baf5ec9d
exelix <13405476+exelix11@users.noreply.github.com>
2023-04-17 12:05:01 +0200

    Match defender ASR rule names with ms docs

--- security/fileassoc.bat
+++ security/fileassoc.bat
@@ -15,15 +15,15 @@ ftype vbsfile="%SystemRoot%\system32\NOTEPAD.EXE" "%1"
 ftype batfile="%SystemRoot%\system32\NOTEPAD.EXE" "%1"
 
 ::PRESET: ADV,SIM
-::CAT: Defender advanced settings
+::CAT: Defender advanced settings (Windows Pro/Enterprise/Education only)
 
-:: Block Office Child Process Creation 
+:: Block Office application from creating child processes 
 powershell.exe Add-MpPreference -AttackSurfaceReductionRules_Ids D4F940AB-401B-4EFC-AADC-AD5F3C50688A -AttackSurfaceReductionRules_Actions Enabled
-:: Block Process Injection
+:: Block Office applications from injecting code into other processes
 powershell.exe Add-MpPreference -AttackSurfaceReductionRules_Ids 75668C1F-73B5-4CF0-BB93-3ECF5CB7CC84 -AttackSurfaceReductionRules_Actions Enabled
-:: Block Win32 API calls in macros
+:: Block Win32 API calls from Office macros
 powershell.exe Add-MpPreference -AttackSurfaceReductionRules_Ids 92E97FA1-2EDF-4476-BDD6-9DD0B4DDDC7B -AttackSurfaceReductionRules_Actions Enabled
-:: Block Office from creating executables
+:: Block Office applications from creating executable content
 powershell.exe Add-MpPreference -AttackSurfaceReductionRules_Ids 3B576869-A4EC-4529-8536-B80A7769E899 -AttackSurfaceReductionRules_Actions Enabled
 :: Block execution of potentially obfuscated scripts
 powershell.exe Add-MpPreference -AttackSurfaceReductionRules_Ids 5BEB7EFE-FD9A-4556-801D-275E5FFC04CC -AttackSurfaceReductionRules_Actions Enabled
@@ -31,14 +31,18 @@ powershell.exe Add-MpPreference -AttackSurfaceReductionRules_Ids 5BEB7EFE-FD9A-4
 powershell.exe Add-MpPreference -AttackSurfaceReductionRules_Ids BE9BA2D9-53EA-4CDC-84E5-9B1EEEE46550 -AttackSurfaceReductionRules_Actions Enabled
 :: Block JavaScript or VBScript from launching downloaded executable content
 powershell.exe Add-MpPreference -AttackSurfaceReductionRules_Ids D3E037E1-3EB8-44C8-A917-57927947596D -AttackSurfaceReductionRules_Actions Enabled
-:: Block lsass cred theft
+:: Block credential stealing from the Windows local security authority subsystem (lsass.exe)
 powershell.exe Add-MpPreference -AttackSurfaceReductionRules_Ids 9e6c4e1f-7d60-472f-ba1a-a39ef669e4b2 -AttackSurfaceReductionRules_Actions Enabled
-:: Block Adobe Reader from creating child processes
+:: Block process creation from Adobe Reader (beta)
 powershell.exe Add-MpPreference -AttackSurfaceReductionRules_Ids 7674ba52-37eb-4a4f-a9a1-f0f9a1619a2c -AttackSurfaceReductionRules_Actions Enabled
 :: Block persistence through WMI event subscription
 powershell.exe Add-MpPreference -AttackSurfaceReductionRules_Ids e6db77e5-3df2-4cf1-b95a-636979351e5b -AttackSurfaceReductionRules_Actions Enabled
 :: Block process creations originating from PSExec and WMI commands
 powershell.exe Add-MpPreference -AttackSurfaceReductionRules_Ids d1e49aac-8f56-4280-b9ba-993a6d77406c -AttackSurfaceReductionRules_Actions Enabled
+
+::PRESET: SIM
+:: Use advanced protection against ransomware
+powershell.exe Add-MpPreference -AttackSurfaceReductionRules_Ids c1db55ab-c21a-4637-bb3f-a12568109d35 -AttackSurfaceReductionRules_Actions Enabled
 
 ::PRESET:
 :: Block untrusted and unsigned processes that run from USB
